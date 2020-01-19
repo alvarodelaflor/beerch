4a012b5a07eb54ffe06f6c9c85c7a65f7cf6fff8
antoniosalvatperez@gmail.com <antoniosalvatperez@gmail.com>
2020-01-19 21:12:45 +0100

    populate first version with errors

--- base/urls.py
+++ base/urls.py
@@ -10,13 +10,7 @@ urlpatterns = [
     path('register', users_view.register),
     path('login', users_view.login),
     path('logout', users_view.logout),
-    path('welcome', users_view.welcome),
     path('', myapp_view.index),
-    path('cv', myapp_view.cv),
-    path('curriculumvitae', myapp_view.cv),
-    path('curriculumVitae', myapp_view.cv),
-    path('CurriculumVitae', myapp_view.cv),
-    path('Curriculumvitae', myapp_view.cv),
-    path('post', myapp_view.post),
     path('admin/', admin.site.urls),
+    path('indexRestaurants',myapp_view.indexRestaurants)
 ]
--- myapp/models.py
+++ myapp/models.py
@@ -5,9 +5,27 @@ class Restaurant(models.Model):
     town = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    link = models.CharField(max_length=100)
     food_types = models.CharField(max_length=100)
     special_diets = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "{0}".format(self.nombre)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.URLField(default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv_p3auZIO-jK7XWsgmAOwOLrSL7GcJ4JT1CQsdJl391lPu8vj&s')
+
+    def __str__(self):
+        return "{0}".format(self.nombre)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
+    rate = models.IntegerField()
+    visit_date = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+       
     def __str__(self):
         return "{0}".format(self.nombre)
